f27104106edc1b426b7e50ba0660e9b57d72f977
Erica Little <eteney90@gmail.com>
2021-04-24 19:17:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
 end
 
   def self.scrape_profile_page(profile_url)
-    profile_page = Nokogiri::HTML(open(index_url))
+    profile_page = Nokogiri::HTML(open(profile_url))
 
     student = {}
 
