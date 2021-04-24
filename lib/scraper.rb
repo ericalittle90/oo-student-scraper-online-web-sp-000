69e9d86d5909928edbb607fda108d4e037813cdb
Erica Little <eteney90@gmail.com>
2021-04-24 19:01:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,9 +10,9 @@ class Scraper
 
     index_page.css("div.student-card").each do |student|
       students = {
-        :name => student.css('.student-name').text,
-        :location => student.css('.student-location').text,
-        :profile_url => student.css("a").attr('href').value
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").attr("href").value
       }
     #h4 class="student-name"
     #p class="student-location"
